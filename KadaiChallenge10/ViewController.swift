a1fc0ddda8bdc0084f312a86e27a53e3a0cb5e97
Sena <senana0116@icloud.com>
2022-05-05 08:35:53 +0900

    課題チャレンジ１０

--- KadaiChallenge10/ViewController.swift
+++ KadaiChallenge10/ViewController.swift
@@ -8,14 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    private let item = ["北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県",
-                "福島県", "茨城県", "栃木県", "群馬県", "埼玉県", "千葉県",
-                "東京都", "神奈川県", "新潟県", "富山県", "石川県", "福井県",
-                "山梨県", "長野県", "岐阜県", "静岡県", "愛知県", "三重県",
-                "滋賀県", "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県",
-                "鳥取県", "島根県", "岡山県", "広島県", "山口県", "徳島県",
-                "香川県", "愛媛県", "高知県", "福岡県", "佐賀県", "長崎県",
-                "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県"]
+    private let prefecturesList = ["北海道", "青森県", "岩手県", "宮城県", "秋田県",
+                                   "山形県","福島県", "茨城県", "栃木県", "群馬県",
+                                   "埼玉県", "千葉県","東京都", "神奈川県", "新潟県",
+                                   "富山県", "石川県", "福井県","山梨県", "長野県",
+                                   "岐阜県", "静岡県", "愛知県", "三重県","滋賀県",
+                                   "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県",
+                                   "鳥取県", "島根県", "岡山県", "広島県", "山口県",
+                                   "徳島県","香川県", "愛媛県", "高知県", "福岡県",
+                                   "佐賀県", "長崎県","熊本県", "大分県", "宮崎県",
+                                   "鹿児島県", "沖縄県"]
 
     @IBOutlet private weak var tableView: UITableView!
 
@@ -26,7 +28,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        item.count
+        prefecturesList.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -48,7 +50,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.register(UINib(nibName: "PrefeturesTableViewCell", bundle: nil), forCellReuseIdentifier: identifier)
 
         let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath) as? PrefeturesTableViewCell
-        cell?.prefecturesNameLabel.text = item[indexPath.row]
+        cell?.prefecturesNameLabel.text = prefecturesList[indexPath.row]
         cell?.prefecturesNumberLabel.text = "\(indexPath.row + 1)番目の都道府県です"
         cell?.backgroundColor = colorRGB
         return cell!
