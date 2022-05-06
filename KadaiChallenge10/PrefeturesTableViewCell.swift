c7183ebdab33ea1a4f32200c4ba47992ff0ec8f7
akio0911 <akio0911@gmail.com>
2022-05-06 19:44:57 +0900

    feedback

--- KadaiChallenge10/PrefeturesTableViewCell.swift
+++ KadaiChallenge10/PrefeturesTableViewCell.swift
@@ -8,6 +8,20 @@
 import UIKit
 
 class PrefeturesTableViewCell: UITableViewCell {
-    @IBOutlet weak var prefecturesNameLabel: UILabel!
-    @IBOutlet weak var prefecturesNumberLabel: UILabel!
+    @IBOutlet private weak var prefecturesNameLabel: UILabel!
+    @IBOutlet private weak var prefecturesNumberLabel: UILabel!
+
+    func configure(name: String, row: Int) {
+        prefecturesNameLabel.text = name
+        prefecturesNumberLabel.text = "\(row + 1)番目の都道府県です"
+
+        switch row % 3 {
+        case 0:
+            backgroundColor = UIColor(red: 255/255, green: 100/255, blue: 100/255, alpha: 100/100)
+        case 1:
+            backgroundColor = UIColor(red: 100/255, green: 255/255, blue: 100/255, alpha: 100/100)
+        default:
+            backgroundColor = UIColor(red: 100/255, green: 100/255, blue: 255/255, alpha: 100/100)
+        }
+    }
 }
